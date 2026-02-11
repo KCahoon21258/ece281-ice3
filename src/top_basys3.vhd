6a00081fd84b710fdbeb2757cc2dedc9e8c6aa3d
c28Kimberly.Cahoon <c28kimberly.cahoon@afacademy.af.edu>
2026-02-11 11:02:59 -0700

    ICE03 Upload

--- src/top_basys3.vhd
+++ src/top_basys3.vhd
@@ -1,4 +1,3 @@
---+----------------------------------------------------------------------------
 --| 
 --| DESCRIPTION   : This file implements the top level module for a BASYS 
 --|
@@ -56,15 +55,35 @@ end top_basys3;
 architecture top_basys3_arch of top_basys3 is 
 	
     -- declare the component of your top-level design
+   component  ripple_adder is
+    Port ( A : in STD_LOGIC_VECTOR (3 downto 0);
+           B : in STD_LOGIC_VECTOR (3 downto 0);
+           Cin : in STD_LOGIC;
+           S : out STD_LOGIC_VECTOR (3 downto 0);
+           Cout : out STD_LOGIC);
+end component ripple_adder;
 
-    -- declare any signals you will need	
+    -- declare any signals you will need
+   	-- declare signals needed to stimulate the UUT inputs
+	signal w_addends     : std_logic_vector(7 downto 0) := x"00"; -- the numbers being added
+	signal w_sum         : std_logic_vector(3 downto 0) := x"0";
+	signal w_Cin, w_Cout : std_logic;
+	
+		
   
 begin
 	-- PORT MAPS --------------------
-   
+ripple_adder_uut : ripple_adder port map (
+	   A    => sw(4 downto 1),
+	   B    => sw(15 downto 12),
+	   Cin  => sw(0),
+	   S    => led(3 downto 0),
+	   Cout => led(15)
+	);
 	---------------------------------
 	
 	-- CONCURRENT STATEMENTS --------
+	
 	led(14 downto 4) <= (others => '0'); -- Ground unused LEDs
 	---------------------------------
 end top_basys3_arch;
